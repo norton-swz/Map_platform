c8b94a9f2f2a16155df08cf1cb72debb0cd25987
norton-swz <1670027520@qq.com>
2024-05-30 12:16:43 +0800

    在仓库中删除script

--- script/copyImages.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-
-src = "E:\\Source\\cpp\\BasicInterface\\BasicInterface\\image\\Level_1\\test.png"
-
-for i in range(1, 41):
-    for j in range(1, 41):
-        dst = "E:\\Source\\cpp\\BasicInterface\\BasicInterface\\Level_1\\" + "Tile_01_" + str(i).zfill(3) + "_" + str(j).zfill(3) + ".png"
-        shutil.copy(src, dst)
