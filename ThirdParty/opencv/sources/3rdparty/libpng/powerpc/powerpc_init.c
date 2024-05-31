9f08ba13f6259659e33bf4fc56243f804915283e
norton-swz <1670027520@qq.com>
2024-05-31 10:42:28 +0800

    添加ThirdParty

---
+++ ThirdParty/opencv/sources/3rdparty/libpng/powerpc/powerpc_init.c
@@ -0,0 +1,16 @@
+
+/* powerpc_init.c - POWERPC optimised filter functions
+ *
+ * Copyright (c) 2018 Cosmin Truta
+ * Copyright (c) 2017 Glenn Randers-Pehrson
+ * Written by Vadim Barkov, 2017.
+ *
+ * This code is released under the libpng license.
+ * For conditions of distribution and use, see the disclaimer
+ * and license in png.h
+ */
+
+/* Below, after checking __linux__, various non-C90 POSIX 1003.1 functions are
+ * called.
+ */
+#defi
